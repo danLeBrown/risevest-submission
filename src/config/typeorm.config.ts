f8fd82cd0a51d48f10592abefb0b0714519117ae
Brown <ayomidedaniel00@gmail.com>
2023-08-26 17:33:56 +0100

    feat: DATABASE_URL

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
+  url: process.env.DATABASE_URL,
   host: process.env.POSTGRES_HOST,
   port: Number(process.env.POSTGRES_PORT),
   username: process.env.POSTGRES_USER,
